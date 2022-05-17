d45b88cacefd76589cbca0824c8ad313311188da
pepelaz <pepelaz101@gmail.com>
2022-05-17 17:00:52 +0700

    creating liquidity pool

--- contracts/Staking.sol
+++ contracts/Staking.sol
@@ -4,31 +4,16 @@ pragma solidity ^0.8.0;
 import "hardhat/console.sol";
 
 contract Staking {
-    // string private greeting;
 
-    // constructor(string memory _greeting) {
-    //     console.log("Deploying a Greeter with greeting:", _greeting);
-    //     greeting = _greeting;
-    // }
-
-    // function greet() public view returns (string memory) {
-    //     return greeting;
-    // }
-
-    // function setGreeting(string memory _greeting) public {
-    //     console.log("Changing greeting from '%s' to '%s'", greeting, _greeting);
-    //     greeting = _greeting;
-    // }
-
-    function stake(uint256 _amount) public {
+    function stake(uint256) public {
 
     }
 
-    function unstake() public {
+    function unstake() pure public {
 
     }
 
-    function claim() public {
+    function claim() pure public {
         
     }
 }
--- scripts/deploy.ts
+++ scripts/deploy.ts
@@ -1,22 +1,52 @@
 
 import { ethers } from "hardhat";
+const { MaxUint256 } = ethers.constants;
+const { parseEther, formatEther } = ethers.utils;
+const routerAddress = process.env.UNISWAP_ROUTER_ADDRESS as string;
+const factoryAddress = process.env.UNISWAP_FACTORY_ADDRESS as string;
+
+
+//router's ABI
+const abiRouter = require('../artifacts/@uniswap/v2-periphery/contracts/interfaces/IUniswapV2Router02.sol/IUniswapV2Router02.json').abi;
+
 
 async function main() {
-
+ 
     // delpoy TokenOne
     const TokenOne = await ethers.getContractFactory("TokenOne");
     const tokenOne = await TokenOne.deploy();
     await tokenOne.deployed();
-
     console.log("TokenOne deployed to:", tokenOne.address);
 
 
-    // delpoy TokenTwo
-    const TokenTwo = await ethers.getContractFactory("TokenTwo");
-    const tokenTwo = await TokenTwo.deploy();
-    await tokenTwo.deployed();
-  
-    console.log("TokenTwo deployed to:", tokenTwo.address);
+    let factory = await ethers.getContractAt("IUniswapV2Factory", factoryAddress);
+    let router = await ethers.getContractAt("IUniswapV2Router02", routerAddress);
+   
+    const wethAddress = await router.WETH()
+
+    // create pair
+    let tx = await factory.createPair(tokenOne.address, wethAddress);  
+    await tx.wait();
+
+    const pairAddress = await factory.getPair(tokenOne.address, wethAddress);  
+    console.log("LpToken address:", pairAddress);
+
+    let lpToken = await ethers.getContractAt("LpToken", pairAddress);
+    
+    // approve
+    await tokenOne.approve(routerAddress, MaxUint256);
+
+   
+    // add liquidity pool    
+    const [addr] = await ethers.provider.listAccounts();
+    console.log("lp token before: ", await lpToken.balanceOf(addr));
+    
+    tx = await router.addLiquidityETH(tokenOne.address,  parseEther('200'),  0, 0, addr, MaxUint256, {
+      value: parseEther('0.01')
+    });
+    await tx.wait();
+
+    console.log("lp token after: ", await lpToken.balanceOf(addr));
  }
 
 main().catch((error) => {
